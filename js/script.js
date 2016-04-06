ea67910b5f565276e375f995b17f38c56641542a
sdiprima-nmdp <sdiprima@nmdp.org>
2016-04-06 14:48:41 -0500

    commit to test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,61 @@
-$.ready(function() {
+$(function() {
     
-   console.log("hello"); 
+    // add button
+   $('#form').append('<button id="fetch">Fetch Weather</button>');
     
+    // convert temp
+    convertKtoF = function(K){
+        return Math.round(((K - 273.15) * 1.8) + 32)
+    }
+    
+    //validate zipcode
+    validateZIP = function(zip){
+        //check that it's 5 digits
+        if (zip.length !== 5){
+            return false;
+        }
+        //check that it's numeric
+        if (isNaN(Number(number1))){
+            return false;
+        }
+        return true;
+    }
+    
+   $('#fetch').on('click',function(){
+       var zipcode = $('#zip').val();
+       var apiKey = $('#apikey input').val();
+       var zipCheck = validateZIP(zipcode);
+       
+       if (zipCheck) {
+       
+           var url = 'http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid='+apiKey;
+
+           $.getJSON(url).done(function(data){
+               
+                   var location = data.name;
+                   var temp = convertKtoF(data.main.temp);
+                   var desc = data.weather[0].description;
+
+                   $('#weather').html(
+                    '<h3><strong>Current City: </strong>'+location+'</h3>'+
+                    '<p><strong>Current Temperature: </strong>'+temp+'&#8457;</p>'+
+                    '<p><strong>Current Weather: </strong>'+desc+'</p>'
+                   );
+
+           }).fail(function(e) {
+                 $('#weather').html(
+                '<h3>Something went wrong...</h3>'+
+                '<p>Code:'+e.cod+'</p>'+
+                '<p>Message:'+e.message+'</p>'
+               );
+           });
+           
+       } else {
+           $('#weather').html(
+                '<h3><strong>Bad Zipcode: </strong>Please enter only 5 digit zipcodes and try again.</h3>'
+               );
+       }
+       
+   })
     
 });
